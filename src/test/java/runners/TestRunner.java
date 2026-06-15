434661bc078462057f00c408536357ec4fcfb7bd
Marwa Essam <m.e4658099@gmail.com>
2026-06-15 18:30:02 +0300

    Add files via upload

---
+++ src/test/java/runners/TestRunner.java
@@ -0,0 +1,25 @@
+package runners;
+
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+import org.testng.annotations.DataProvider;
+
+@CucumberOptions(
+        features = "src/test/resources/features",
+        glue = {"steps", "hooks"},
+        plugin = {
+                "pretty",
+                "html:target/cucumber-reports/cucumber.html"
+        },
+        monochrome = true
+)
+
+public class TestRunner extends AbstractTestNGCucumberTests {
+
+    @Override
+    @DataProvider(parallel = true)
+    public Object[][] scenarios() {
+        return super.scenarios();
+    }
+}
